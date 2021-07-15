7790be21457422d7bb2bf69b030f6ea1b657e7bb
Anand <f20190061@goa.bits-pilani.ac.in>
2021-07-15 12:05:34 +0530

    July 15, 2021 12:05 PM

--- ques.cpp
+++ ques.cpp
@@ -1,9 +1,9 @@
-#code to add 2 numbers
-
+#include <bits/stdc++.h>
+using namespace std;
 int main()
 {
-    int a=1
-    int b=2
-    int c=?
-    ?<<c
+    int a=1;
+    int b=2;
+    int c=a+b+0;
+    cout<<c;
 }
